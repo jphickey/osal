ee4650b42709c9c6090a905d6ce63edab757169a
Joseph Hickey <joseph.p.hickey@nasa.gov>
2026-08-21 11:16:20 -0400

    Rework stubs

--- src/unit-test-coverage/ut-stubs/inc/OCS_string.h
+++ src/unit-test-coverage/ut-stubs/inc/OCS_string.h
@@ -40,7 +40,7 @@
 /* prototypes normally declared in string.h */
 /* ----------------------------------------- */
 
-extern void  *OCS_memchr(const void *s, int c, size_t n);
+extern const void  *OCS_memchr(const void *s, int c, size_t n);
 extern void  *OCS_memcpy(void *dest, const void *src, size_t n);
 extern void  *OCS_memset(void *s, int c, size_t n);
 extern int    OCS_strcmp(const char *s1, const char *s2);
@@ -48,9 +48,9 @@ extern char  *OCS_strcpy(char *dest, const char *src);
 extern size_t OCS_strlen(const char *s);
 extern int    OCS_strncmp(const char *s1, const char *s2, size_t n);
 extern char  *OCS_strncpy(char *dest, const char *src, size_t n);
-extern char  *OCS_strchr(const char *s, int c);
-extern char  *OCS_strrchr(const char *s, int c);
-extern char  *OCS_strstr(const char *haystack, const char *needle);
+extern const char  *OCS_strchr(const char *s, int c);
+extern const char  *OCS_strstr(const char *haystack, const char *needle);
+extern const char  *OCS_strrchr(const char *s, int c);
 extern char  *OCS_strcat(char *dest, const char *src);
 extern char  *OCS_strncat(char *dest, const char *src, size_t n);
 extern char  *OCS_strerror(int errnum);
